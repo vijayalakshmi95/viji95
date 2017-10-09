3f48a975838efa21851c183e2647a8a999d2accf
vijayalakshmi95 <32513207+vijayalakshmi95@users.noreply.github.com>
2017-10-09 15:26:34 +0530

    Add files via upload

---
+++ Cd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Cd
+{
+public static void main(String[] args)
+{
+List a1 =new ArrayList();
+a1.add("amaz");
+a1.add("lana");
+System.out.println("Araay list");
+System.out.println("\t "+ a1);
+List a2 =new ArrayList();
+a2.add("amaz");
+a2.add("lana");
+System.out.println("Araay list");
+System.out.println("\t "+ a2);
+
+}
+}
